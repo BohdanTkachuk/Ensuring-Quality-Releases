6aa39a1fc1ae7193ead62506a8b55b1d6055d161
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-12 20:23:53 +0100

    Update build.sh

--- automatedtesting/jmeter/build.sh
+++ automatedtesting/jmeter/build.sh
@@ -11,7 +11,7 @@ gunzip apache-jmeter-${VERSION}.tgz
 # Build docker image containing jmeter.
 docker build . --tag jmeter:latest
 # Run docker container.
-docker run -d --name jmeter -it jmeter
+docker run -d --name jmeter -it jmeter:latest
 docker rm -f jmeter
 sleep $(($DURATION + 60))
-docker cp jmeter:/LoadReports .
+docker cp jmeter:latest:/LoadReports .
