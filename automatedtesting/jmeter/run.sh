e8002eff0daafb62dc96730ec189607105752115
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-12 21:01:56 +0100

    Update run.sh

--- automatedtesting/jmeter/run.sh
+++ automatedtesting/jmeter/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION='5.3'
-mkdir LoadReports
+echo "The jmeter will work next"
 ./apache-jmeter-${VERSION}/bin/jmeter -n -t Starter.jmx -l LoadReports\results.jtl -e -o LoadReports
-sleep 180
+
