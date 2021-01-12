f828244501825be8bef73a5e70c6d671f2d792dc
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-12 21:25:42 +0100

    Update build.sh

--- automatedtesting/jmeter/build.sh
+++ automatedtesting/jmeter/build.sh
@@ -16,5 +16,6 @@ docker run -d --name jmeter -it jmeter:latest
 # sleep $(($DURATION + 60))
 docker ps -a 
 docker cp jmeter:/LoadReports .
-ls jmeter:/
+ls jmeter:/LoadReports -a
+ls jmeter:/. -a
 
