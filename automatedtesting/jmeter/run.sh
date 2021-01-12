49b8ea0342504937d4ec01f667d2b27fabad39ca
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-12 21:12:34 +0100

    Update run.sh

--- automatedtesting/jmeter/run.sh
+++ automatedtesting/jmeter/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 VERSION='5.3'
+ls -all
 echo "The jmeter will work next"
-ls -a
+mkdir LoadReports
 ./apache-jmeter-${VERSION}/bin/jmeter -n -t Starter.jmx -l LoadReports\results.jtl -e -o LoadReports
 echo "Finishing jmeter" 
 ls -a
