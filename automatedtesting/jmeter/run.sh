afd96ebd6e8f0995c06a6d0122d0cf9a8fc02f50
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-12 16:27:35 +0100

    Update run.sh

--- automatedtesting/jmeter/run.sh
+++ automatedtesting/jmeter/run.sh
@@ -8,6 +8,6 @@ ROOTPATH=$1
 
 echo "$ROOTPATH"
 # Finally run
-docker stop $NAME > /dev/null 2>&1
-docker rm $NAME > /dev/null 2>&1
-docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
+sudo docker stop $NAME > /dev/null 2>&1
+sudo docker rm $NAME > /dev/null 2>&1
+sudo docker run --name $NAME -i -v $ROOTPATH:/test -w /test $IMAGE ${@:2}
