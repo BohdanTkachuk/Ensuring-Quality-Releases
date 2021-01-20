c17d3c0f35e4761321d0034652ba3b37bfe4c567
BohdanTkachuk <49440230+BohdanTkachuk@users.noreply.github.com>
2021-01-20 19:33:02 +0100

    Update login.py

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,6 +1,6 @@
 # #!/usr/bin/env python
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Opoptionstions as ChromeOptions
 
 
 # Start the browser and login with standard_user
@@ -8,7 +8,7 @@ def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
-    options.add_argument("--no-sandbox")
+    #options.add_argument("--no-sandbox")
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
