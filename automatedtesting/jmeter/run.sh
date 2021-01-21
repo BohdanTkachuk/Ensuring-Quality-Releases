ea1e349be25ce57cbf92cd3cd62f6b0d1bbe34aa
bohdan <tkachuk_bohdan@ukr.net>
2021-01-21 21:50:48 +0100

    added jmeter

--- automatedtesting/jmeter/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-VERSION='5.3'
-ls -all
-echo "The jmeter will work next"
-./apache-jmeter-${VERSION}/bin/jmeter -n -t Starter.jmx -l report/results.jtl 
-echo "Finishing jmeter" 
-ls -a
